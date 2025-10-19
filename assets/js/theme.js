bdbfb2a0783cddad41b96888263e105f13d48fdf
Alejandro E. Rendon <alejandro@rendon.co>
2025-10-19 18:34:33 -0500

    Refactor page.html to use layout and title metadata; create new projects.html for project showcase with responsive design and project descriptions.

---
+++ assets/js/theme.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "brand-blue": "#55efc4", // Green Darner Tail
+        "brand-violet": "#a29bfe", // Shy Moment
+        "brand-dark-blue": "#2d3436", // Dracula Orchid
+        "brand-light-gray": "#f9fafb",
+        "brand-secondary-blue": "#0984e3", // Electron Blue
+        "brand-text-dark": "#636e72", // American River
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
